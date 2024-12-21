08d5d8b0ab41f4b00990564137fc65ea81bf330a
Rhet Turnbull <rturnbull@gmail.com>
2024-12-21 07:08:35 -0600

    Python 3.13 support

--- setup.py
+++ setup.py
@@ -59,15 +59,15 @@ setup(
     install_requires=[
         "click>=8.0.1",
         "cloup>=0.11.0",
-        "pathvalidate>=2.5.0,<3.0.0",
+        "pathvalidate>=2.5.0,<4.0.0",
         "pdfminer.six>=20211012",
         "pluggy>=1.0.0,<=2.0.0",
         "python-docx>=0.8.11,<2.0.0",
         "rich>=11.0.0",
-        "tenacity>=8.0.1,<9.0.0",
+        "tenacity>=8.0.1,<10.0.0",
         "textX>=2.3.0",
-        "tinytag>=1.8.0,<2.0.0",
-        "yaspin>=2.1.0,<3.0.0",
+        "tinytag>=1.8.0,<3.0.0",
+        "yaspin>=2.1.0,<4.0.0",
     ]
     + (
         [
